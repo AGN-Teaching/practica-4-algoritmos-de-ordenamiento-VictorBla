13dae3ae406cff2b9b1c417a86dafbdae635c5cd
VictorBla <151650061+VictorBla@users.noreply.github.com>
2024-01-30 12:07:34 -0600

    Update ordenamiento_rec_t.h

--- ordenamiento_rec_t.h
+++ ordenamiento_rec_t.h
@@ -1,12 +1,14 @@
+#ifndef ORDENAMIENTO_REC_T_H
+#define ORDENAMIENTO_REC_T_H
+
+#include "ordenamiento_t.h"
+
 void merge(int A[], int inicio, int medio, int fin) {
     int *aux = new int[fin - inicio + 1];
-    int i = inicio;     // índice de la primera mitad
-    int j = medio + 1;  // índice de la segunda mitad
+    int i = inicio;
+    int j = medio + 1;
     int h = 0;
 
-    // Se recorren ambos subarreglos y se van combinando sus
-    // elementos ordenadamente hasta que se haya recorrido
-    // totalmente uno de ellos
     while ((i <= medio) && (j <= fin)) {
         if (A[i] <= A[j]) {
             aux[h] = A[i];
@@ -18,16 +20,13 @@ void merge(int A[], int inicio, int medio, int fin) {
         h += 1;
     }
 
-    // Si se copiaron todos los elementos de la primera mitad,
-    // se copia el resto de la segunda mitad
     if (i > medio) {
         while (j <= fin) {
             aux[h] = A[j];
             j += 1;
             h += 1;
         }
-    } // Si no, se copia el resto de la primera mitad
-    else {
+    } else {
         while (i <= medio) {
             aux[h] = A[i];
             i += 1;
@@ -52,26 +51,10 @@ void merge_sort(int A[], int inicio, int fin) {
     }
 }
 
-
-
-
-
-void intercambiar(int A[], int x, int y) {
-    int aux = A[x];
-    A[x] = A[y];
-    A[y] = aux;
-}
-
 int partition(int A[], int p, int r) {
-    // A[r] es el registro elegido cuya llave será el pivote
     int pivote = A[r];
-    
-    // Indica el índice del último registro cuyo valor
-    // es menor a pivote
     int ultimo = p - 1;
 
-    // Se agrupan los elementos menores a pivote en las primeras
-    // posiciones de A
     for (int i = p; i < r; i++) {
         if (A[i] < pivote) {
             ultimo += 1;
@@ -79,7 +62,6 @@ int partition(int A[], int p, int r) {
         }
     }
 
-    // Y se mueve al pivote a su posición correcta
     intercambiar(A, ultimo + 1, r);
     return ultimo + 1;
 }
@@ -91,3 +73,5 @@ void quicksort(int A[], int p, int r) {
         quicksort(A, q + 1, r);
     }
 }
+
+#endif /* ORDENAMIENTO_REC_T_H */
